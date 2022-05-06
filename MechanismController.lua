55ea4659874acb1fcf48899f5ea1d3ba56884aab
felipe-aguirre <github@aguirre.in>
2022-05-05 22:02:29 -0400

    .

--- MechanismController.lua
+++ MechanismController.lua
@@ -1,7 +1,30 @@
+entradaIzquierda = "left"
+entradaDerecha = "right"
+salida = "front"
+iluminacion = "bottom"
+
+
+-- Limpia las salidas de redstone a valor 0
+function cleanInput()
+  rs.setBundledOutput(salidas,0)
+  rs.setBundledOutput(iluminacion,0)
+end
+
+cleanOutputs()
+
 while true do
-  salidas = "front"
-  rs.setBundledOutput(salidas, colors.yellow)
-  sleep(2)
-  rs.setBundledOutput(salidas,colors.magenta)
-  sleep(2)
+  -- KM = KineticMechanism
+  KMBoton = rs.getBundledInput(entradaIzquierda, colors.yellow)
+
+  if KMBoton then
+    rs.setBundledOutput(salidas, colors.yellow)
+    sleep(2)
+    rs.setBundledOutput(salidas,colors.magenta)
+    sleep(2)
+  else 
+    rs.setBundledOutput(salidas, 0)
+  end
+
+  -- PM = Precision Mechanism
+
 end
